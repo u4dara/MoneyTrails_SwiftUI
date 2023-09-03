dc7cb1445665b893f33f8b5284caeb5601860456
Udara Sachinthana <u4dara@protonmail.com>
2023-09-03 20:24:21 +0530

    initial commit

---
+++ MoneyTrails_SwiftUI/MoneyTrails_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTrails_SwiftUIApp.swift
+//  MoneyTrails_SwiftUI
+//
+//  Created by Udara Sachinthana on 2023-09-03.
+//
+
+import SwiftUI
+
+@main
+struct MoneyTrails_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
